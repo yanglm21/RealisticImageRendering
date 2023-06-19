b1391967cedc9209ebdc72b5fb804aa4a5677450
ylm <yanglm21@mails.tsinghua.edu.cn>
2023-06-19 12:36:34 +0800

    rewrite sphere

--- include/sphere.hpp
+++ include/sphere.hpp
@@ -37,12 +37,29 @@ public:
         float t_prime = r_square - d * d;
         float t = tp - t_prime;
         if (t > tmin && t < h.getT()){
-            Vector3f normal = (t * Rd + origin - center).normalized();
-            h.set(t, material, normal);
+            Vector3f OP(t * Rd + origin - center);
+            Vector3f normal = OP.normalized();
+            float u = 0.5 + atan2(normal.x(), normal.z()) / (2 * M_PI);
+            float v = 0.5 - asin(normal.y()) / M_PI;
+            h.set(t, material, getNormal(normal, OP, u, v),
+              material->getColor(u, v), t * Rd + origin);
             return true;
         } else {
             return false;
         }
+    }
+
+    Vector3f getNormal(const Vector3f &n, const Vector3f &p, float u, float v) {
+        Vector2f grad(0);
+        float f = material->bump.getDisturb(u, v, grad);
+        if (fabs(f) < FLT_EPSILON) return n;
+        float phi = u * 2 * M_PI, theta = M_PI - v * M_PI;
+        Vector3f pu(-p.z(), 0, p.x()),
+            pv(p.y() * cos(phi), -radius * sin(theta), p.y() * sin(phi));
+        if (pu.squaredLength() < FLT_EPSILON) return n;
+        return Vector3f::cross(pu + n * grad[0] / (2 * M_PI),
+                               pv + n * grad[1] / M_PI)
+            .normalized();
     }
 
 protected:
